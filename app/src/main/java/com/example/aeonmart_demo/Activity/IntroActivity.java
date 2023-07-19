2b5993957cad5a7f0f537c2cad3f2c1ce2a8ef78
felixquang <tranquang071203@gmail.com>
2023-07-19 15:59:05 +0700

    Tạo được IntroActivity

--- app/src/main/java/com/example/aeonmart_demo/Activity/IntroActivity.java
+++ app/src/main/java/com/example/aeonmart_demo/Activity/IntroActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.aeonmart_demo.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_intro);
     }
 }
