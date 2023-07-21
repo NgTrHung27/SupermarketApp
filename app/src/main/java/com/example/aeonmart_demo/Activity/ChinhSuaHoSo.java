476344478831ede864745e5bd587fb9791cb4ba2
Huongcute <117435627+Huongcutee@users.noreply.github.com>
2023-07-21 19:55:58 +0700

    chinh sua hồ sơ

---
+++ app/src/main/java/com/example/aeonmart_demo/Activity/ChinhSuaHoSo.java
@@ -0,0 +1,16 @@
+package com.example.aeonmart_demo.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.aeonmart_demo.R;
+
+public class ChinhSuaHoSo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chinh_sua_ho_so);
+    }
+}
