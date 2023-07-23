ffd4174d2a35aff9714e1a57b407a874b5941650
PhuCuong2512 <131945083+PhuCuong2512@users.noreply.github.com>
2023-07-23 14:06:56 +0700

    Đăng nhập + Đăng ký - Cường

--- app/src/main/java/com/example/aeonmart_demo/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/aeonmart_demo/Activity/RegisterActivity.java
@@ -1,16 +1,78 @@
 package com.example.aeonmart_demo.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.aeonmart_demo.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText edtName, edtBirth, edtEmail, edtPassword, edtPhone, edtCCCD, edtAddress;
+    private Button btnRegister;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Initialize Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // Bind the views
+        edtName = findViewById(R.id.Regis_EdText_Name);
+        edtBirth = findViewById(R.id.Regis_EdText_Birth);
+        edtEmail = findViewById(R.id.Regis_EdText_Em);
+        edtPassword = findViewById(R.id.Regis_EdText_MK);
+        edtPhone = findViewById(R.id.Regis_EdText_Sdt);
+        edtCCCD = findViewById(R.id.Regis_EdText_CCCD);
+        edtAddress = findViewById(R.id.Regis_EdText_diachi);
+        btnRegister = findViewById(R.id.profile_BTN_caidat);
+
+        // Set click listener for the "Đăng ký" button
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+    }
+
+    private void registerUser() {
+        String email = edtEmail.getText().toString().trim();
+        String password = edtPassword.getText().toString().trim();
+
+        // You can add more validation checks for other fields if needed
+
+        // Create a new user in Firebase Authentication
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Registration successful
+                            Intent intent  = new  Intent(getApplicationContext(), SignInActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(RegisterActivity.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+                            // You can do further operations here, such as saving user data to Firestore, etc.
+                            // For simplicity, I'm not including that part in this code snippet.
+                        } else {
+                            // Registration failed
+                            Toast.makeText(RegisterActivity.this, "Đăng ký thất bại: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/aeonmart_demo/Activity/SignInActivity.java
+++ app/src/main/java/com/example/aeonmart_demo/Activity/SignInActivity.java
@@ -2,15 +2,89 @@ package com.example.aeonmart_demo.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.aeonmart_demo.R;
+import com.google.firebase.FirebaseApiNotAvailableException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthActionCodeException;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 public class SignInActivity extends AppCompatActivity {
+    private EditText edtEmail, edtPassword;
+    private Button btnSignIn, btnSignUp;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // Ánh xạ các view từ file XML
+        edtEmail = findViewById(R.id.Signin_EDT_Email);
+        edtPassword = findViewById(R.id.Signin_EDT_PASS);
+        btnSignIn = findViewById(R.id.Signin_BTN_Signin);
+        btnSignUp = findViewById(R.id.Signin_BTN_SignUp);
+        // Xử lý sự kiện khi người dùng nhấn nút Đăng nhập
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lấy thông tin từ EditText
+                String email = edtEmail.getText().toString().trim();
+                String password = edtPassword.getText().toString().trim();
+
+                // Kiểm tra xem thông tin đăng nhập có hợp lệ hay không
+                if (isValid(email, password)) {
+                    // Đăng nhập bằng Firebase
+                    firebaseAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(SignInActivity.this, task -> {
+                                if (task.isSuccessful()) {
+                                    // Đăng nhập thành công
+                                    Intent intent  = new  Intent(getApplicationContext(), Detail_Profile_Activity.class);
+                                    startActivity(intent);
+                                    Toast.makeText(SignInActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                                    // Ở đây, bạn có thể chuyển đến màn hình chính hoặc thực hiện các hành động khác sau khi đăng nhập thành công
+                                } else {
+                                    // Đăng nhập thất bại, hiển thị thông báo lỗi tương ứng
+                                    try {
+                                        throw task.getException();
+                                    } catch (FirebaseAuthInvalidUserException e) {
+                                        Toast.makeText(SignInActivity.this, "Tài khoản không tồn tại!", Toast.LENGTH_SHORT).show();
+                                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                                        Toast.makeText(SignInActivity.this, "Mật khẩu không đúng!", Toast.LENGTH_SHORT).show();
+                                    } catch (Exception e) {
+                                        Toast.makeText(SignInActivity.this, "Đăng nhập thất bại!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                } else {
+                    // Nếu thông tin đăng nhập không hợp lệ, hiển thị thông báo lỗi
+                    Toast.makeText(SignInActivity.this, "Thông tin đăng nhập không hợp lệ!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new  Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    // Hàm kiểm tra tính hợp lệ của email và password
+    private boolean isValid(String email, String password) {
+        // Kiểm tra logic đăng nhập hợp lệ của bạn tại đây (ví dụ: không để trống, độ dài tối thiểu, v.v.)
+        // Trong ví dụ này, tôi chỉ kiểm tra xem cả hai trường có được điền vào hay không.
+        return !email.isEmpty() && !password.isEmpty();
     }
 }
