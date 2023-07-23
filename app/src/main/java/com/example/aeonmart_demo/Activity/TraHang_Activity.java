50824da70b3ca98ca2d0c8d2ea2e5a2541cd6d00
felixquang <tranquang071203@gmail.com>
2023-07-23 14:15:27 +0700

    sửa java đánh giá + thêm acti trả hàng + item trả hàng

--- app/src/main/java/com/example/aeonmart_demo/Activity/TraHang_Activity.java
+++ app/src/main/java/com/example/aeonmart_demo/Activity/TraHang_Activity.java
@@ -1,17 +1,16 @@
 package com.example.aeonmart_demo.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+
 import com.example.aeonmart_demo.R;
 
-public class Danh_Gia_SP_Activity extends AppCompatActivity {
+public class TraHang_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_danh_gia_sp);
+        setContentView(R.layout.activity_tra_hang);
     }
-
 }
