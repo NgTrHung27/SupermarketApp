4d3e5f8969f540acdddf8197386fe13c493949c8
Huongcute <117435627+Huongcutee@users.noreply.github.com>
2023-07-25 19:05:56 +0700

    ByHuong: huong lam moi thu

--- app/src/main/java/com/example/aeonmart_demo/Activity/DetailActivity.java
+++ app/src/main/java/com/example/aeonmart_demo/Activity/DetailActivity.java
@@ -3,14 +3,43 @@ package com.example.aeonmart_demo.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.aeonmart_demo.R;
+import com.example.aeonmart_demo.utils.Utils;
+
+import org.w3c.dom.Text;
 
 public class DetailActivity extends AppCompatActivity {
-
+    ImageView img;
+    Text name;
+    Button btnThemGH;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        initView();
+        initData();
+
+
+    }
+
+    private void initView() {
+        img = findViewById(R.id.Detail_Img1)
+    }
+
+    private void initData() {
+        btnThemGH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                themGioHang();
+            }
+        });
+    }
+    private void themGioHang()
+    {
+        if (Utils.manggiohang)
     }
 }
---
+++ app/src/main/java/com/example/aeonmart_demo/utils/Utils.java
@@ -0,0 +1,10 @@
+package com.example.aeonmart_demo.utils;
+
+import com.example.aeonmart_demo.Model.GioHang;
+
+import java.util.List;
+
+public class Utils {
+    public static final String BASE_URL = "";
+    public static List<GioHang> manggiohang;
+}
