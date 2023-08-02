5b4a36355d7e07c5688fea7b3d497b00cab67e3f
PhuCuong2512 <131945083+PhuCuong2512@users.noreply.github.com>
2023-08-02 18:35:54 +0700

    Thông báo + Tích xu xml - Cường

---
+++ app/src/main/java/com/example/aeonmart_demo/Activity/NotificationActivity.java
@@ -0,0 +1,16 @@
+package com.example.aeonmart_demo.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.aeonmart_demo.R;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+}
