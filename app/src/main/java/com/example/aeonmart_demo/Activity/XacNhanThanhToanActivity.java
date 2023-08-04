d5aa2b5eac3fc012a62befce29eacc791f1551e9
felixquang <tranquang071203@gmail.com>
2023-08-04 15:30:25 +0700

    Select PTTT

--- app/src/main/java/com/example/aeonmart_demo/Activity/XacNhanThanhToanActivity.java
+++ app/src/main/java/com/example/aeonmart_demo/Activity/XacNhanThanhToanActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageSwitcher;
 import android.widget.TextView;
 
 import com.example.aeonmart_demo.Adapter.XacNhanThanhToanAdapter;
@@ -19,6 +23,15 @@ import java.util.List;
 
 public class XacNhanThanhToanActivity extends AppCompatActivity {
 
+    private boolean isMoMoSelected = false;
+    private boolean isTienMatSelected = false;
+    private boolean isTechcombankSelected = false;
+
+    private ImageSwitcher imgMoMo;
+    private ImageSwitcher imgTienMat;
+    private ImageSwitcher imgTechcombank;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,12 +44,69 @@ public class XacNhanThanhToanActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
 
         TextView tvTongGia = findViewById(R.id.XNTT_tvGia);
+        Button XacNhan = findViewById(R.id.XNTT_btn_XacNhan);
+        XacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(XacNhanThanhToanActivity.this, BillActivity.class);
+                startActivity(i);
+            }
+        });
+
 
         // Nhận giá trị totalPrice từ Intent và gán cho XNTT_tvGia
         double totalPrice = getIntent().getDoubleExtra("totalPrice", 0.0);
         tvTongGia.setText(String.format("%.0fĐ", totalPrice));
+
+        // Khởi tạo ImageSwitcher và bắt sự kiện khi người dùng chọn
+        imgMoMo = findViewById(R.id.XNTT_imgMoMo);
+        imgMoMo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectMoMo();
+            }
+        });
+
+        imgTienMat = findViewById(R.id.XNTT_imgtienmat);
+        imgTienMat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectTienMat();
+            }
+        });
+
+        imgTechcombank = findViewById(R.id.XNTT_imgTechcombank);
+        imgTechcombank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectTechcombank();
+            }
+        });
+    }
+    private void selectMoMo() {
+        isMoMoSelected = true;
+        isTienMatSelected = false;
+        isTechcombankSelected = false;
+        updateUI();
     }
 
+    private void selectTienMat() {
+        isMoMoSelected = false;
+        isTienMatSelected = true;
+        isTechcombankSelected = false;
+        updateUI();
+    }
 
+    private void selectTechcombank() {
+        isMoMoSelected = false;
+        isTienMatSelected = false;
+        isTechcombankSelected = true;
+        updateUI();
+    }
 
+    private void updateUI() {
+        imgMoMo.setBackgroundResource(isMoMoSelected ? R.drawable.logomomo_selected : R.drawable.logomomo);
+        imgTienMat.setBackgroundResource(isTienMatSelected ? R.drawable.thanhtoantienmat_selected : R.drawable.thanhtoantienmat);
+        imgTechcombank.setBackgroundResource(isTechcombankSelected ? R.drawable.logotechcombank_selected : R.drawable.logotechcombank);
+    }
 }
